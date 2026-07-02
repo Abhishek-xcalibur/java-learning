941e650530b75a8f9d95eddb008197f9f46bb5d9
Abhishek Chand <abhishekchand@Abhisheks-MacBook-Air.local>
2026-07-02 21:38:56 +0530

    Arrays

---
+++ Arrays/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        String[] names={"Max","John","Peter"};
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+    }
+}
