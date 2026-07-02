8614bcdb0bd43c68ecd0e63f74bc91388a7e09d4
Abhishek Chand <abhishektr1822@gmail.com>
2026-07-02 21:42:41 +0530

    Add comment on array usage in Main.java
    Added a comment explaining array usage and declaration.

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -6,3 +6,6 @@ public class Main{
         System.out.println(names[2]);
     }
 }
+/*Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+To declare an array, define the variable type with square brackets [ ] : */
